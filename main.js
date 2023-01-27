5852b0bc59ec1846724082d6888ffc88d17686dc
Karolína Lauferová <duskova.karolina@email.cz>
2023-01-27 15:17:14 +0100

    prvnicommit

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 // tady je místo pro náš program
+
+//při najetí myší text ztuční
+function priNajeti() {
+    let odstavec = document.querySelector(".odstavec");
+    odstavec.style.fontWeight = "bold"
+}
+
+let odstavec = document.querySelector(".odstavec");
+
+function cervenyPoKliknuti(event) {
+    odstavec.classlist.add(".cervena");
+    console.log(event);
+}
+
+function vetsiPoKliknuti(event) {
+    odstavec.style.fontSize = "17px";
+    console.log(event)
+}
+
+//nastavení audio
+let prehravac = document.querySelector(".mujPrehravac")
+
+function spustAudio() {
+    prehravac.play();
+}
+
+function zastavAudio() {
+    prehravac.pause();
+}
+
+function resetAudio() {
+    prehravac.currentTime() = 0;
+}
